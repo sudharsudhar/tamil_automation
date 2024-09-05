d76b457a5c2afaa53291f7015fc443dcaae23790
Sudharsan S <52055923+sudharsudhar@users.noreply.github.com>
2024-09-05 23:08:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Ramayanam --> Jai Sri RAM & Sita & Lava & Kusa'
+    return 'Ramayanam --> Jai Sri RAM & Sita & Lava & Kusa --> Hunuman'
     
 
 if __name__ == '__main__':
